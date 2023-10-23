16087edc55dfcc890c054d0ecdb2ef112dad582d
meshnehemia <meshnehemia7@gmail.com>
2023-10-23 10:56:29 +0300

    this touches on polymophism

---
+++ polymophism.py
@@ -0,0 +1,20 @@
+class Shape:
+    def draw(self):
+        print("you are drawing a shape")
+
+
+class Rectangle(Shape):
+    def draw(self):
+        print("drawing the rectangle")
+
+
+class Square(Shape):
+    def draw(self):
+        print("drawing a square")
+
+
+r = Rectangle()
+s = Square()
+
+r.draw()
+s.draw()
